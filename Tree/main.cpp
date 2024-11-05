81a3866e5bf20ffc0455cde82ce85954dd397bc5
varchunn <varvaram186@gmail.com>
2024-11-05 23:58:54 +0200

    Initial Commit

---
+++ Tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tree
+//
+//  Created by Варя Мінєнкова on 05.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
